44d23e8a020191a2b7f55be786c3bd0b5be4e88f
bawrostom <rostombaw2019@gmail.com>
2023-08-22 12:21:31 +0200

    assignment3-part2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -21,7 +21,7 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+mkdir  -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -35,10 +35,18 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	#applying the git patch
+	git apply ~/Downloads/dtc-multiple-definition.patch 
+	#cleaning
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+	#defconfig
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+	#building the kernel
+	make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
 fi
 
 echo "Adding the Image in outdir"
-
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}/
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -46,35 +54,72 @@ then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
-
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
 # TODO: Create necessary base directories
+	mkdir -p bin etc home dev lib lib64 proc sys sbin tmp usr
+	mkdir -p usr/bin usr/lib usr/sbin
+	mkdir -p var/log
+	cd ../
 
-cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+	make menuconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-
+	make distclean
+	make defconfig 
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
+	make CONFIG_PREFIX=../rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}  install 
+cd ../rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
-
+cd ../rootfs/
 # TODO: Add library dependencies to rootfs
+if [ ! -e "lib/ld-linux-aarch64.so.1" ]
+then
+	cp /home/rostom/Downloads/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 lib/
+fi
+if [ ! -e "lib64/libc.o.6" ]
+then
+	cp /home/rostom/Downloads/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 lib64/ 
+fi
+if [ ! -e "lib64/libm.o.6" ]
+then
+	cp /home/rostom/Downloads/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 lib64/
 
+fi
+if [ ! -e "lib64/libresolv.o.6" ]
+then
+	cp /home/rostom/Downloads/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 lib64/
+fi
 # TODO: Make device nodes
+
+	sudo mknod -m 666 dev/null c 1 3
+	sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
 
+cd ~/embedded_linux/assignment-1-bawrostomc/finder-app
+make clean
+make CROSS_COMPILE=aarch64-none-linux-gnu-
 # TODO: Copy the finder related scripts and executables to the /home directory
+cp writer finder.sh finder-test.sh ${OUTDIR}/rootfs/home
+cd ${OUTDIR}/rootfs
 # on the target rootfs
 
 # TODO: Chown the root directory
-
+sudo chown root:root .
 # TODO: Create initramfs.cpio.gz
+
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
+
